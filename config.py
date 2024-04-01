425733e04dbeb5a52913919ba41c427fa0bc30da
TOXICOP638 <149950962+TOXICOP638@users.noreply.github.com>
2024-04-01 20:21:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,9 +26,9 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-SUDO_USERS.append()
-SUDO_USERS.append()
-SUDO_USERS.append()
+SUDO_USERS.append(1939017724)
+SUDO_USERS.append(1338679175)
+SUDO_USERS.append(6350295776)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
